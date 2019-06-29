958bbd8f08aeae0c693761d6515960361ff75cd3
Jon Weatherspoon <jon@Jons-MacBook-Pro.local>
2019-06-29 10:25:51 -0700

    new upgrader behavior

---
+++ savethecontroller.js
@@ -0,0 +1,38 @@
+class UpgraderBehavior {
+    get isMining() {
+        return this._creep.memory.isMining;
+    }
+
+    set isMining(value) {
+        if (this._creep) {
+            this._creep.memory.isMining = value;
+        }
+    }
+
+    run(creep) {
+        this._creep = creep;
+
+        // if we are mining, go mine dawg
+        if (this._creep.carry.energy < 20 && this.isMining) {
+            let sources = this._creep.room.find(FIND_SOURCES);
+            if (this._creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+                this._creep.moveTo(sources[0]);
+            }
+        }
+
+        // If we aren't at full carry capacity we aren't mining 
+        // But if we aren't mining and we have no energy then we gonna mine dawg 
+        this.isMining = this._creep.carry.energy < 20 || (!this.isMining && this._creep.energy === 0);
+
+        // we are DELIVERING 
+        if (!this.isMining) {
+            if (this._creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                this._creep.moveTo(Game.spawns.Spawn1);
+            }
+        }
+
+        this._creep = null;
+    }
+}
+
+module.exports = UpgraderBehavior;
