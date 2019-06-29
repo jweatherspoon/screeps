63eff5c7ff897106b2250d9db5a7f34c5d3bc6ea
jweatherspoon <jweatherspoon@nevada.unr.edu>
2019-06-28 20:45:18 -0700

    upgraders become harvesters now

--- meme/UpgraderBehavior.js
+++ meme/UpgraderBehavior.js
@@ -2,7 +2,19 @@ var ScreepBehavior = require("ScreepBehavior");
 
 class UpgraderBehavior extends ScreepBehavior {
     run(creep) {
+        this._creep = creep;
 
+        if (this._creep.energy == 0) {
+            this._creep.memory.role = "harvester";
+        }
+
+        let controller = this._creep.room.controller;
+        let attempt = this._creep.transfer(controller, RESOURCE_ENERGY);
+        if (attempt == ERR_NOT_IN_RANGE) {
+            this._creep.moveTo(controller);
+        }
+
+        this._creep = null;
     }
 }
 
