ab8674d76ce7340f5dea8f6226983d033572a35b
jweatherspoon <jweatherspoon@nevada.unr.edu>
2019-06-29 10:52:18 -0700

    upgrader fix

--- meme/savethecontroller.js
+++ meme/savethecontroller.js
@@ -13,7 +13,7 @@ class UpgraderBehavior {
         this._creep = creep;
 
         // if we are mining, go mine dawg
-        if (this._creep.carry.energy < 20 && this.isMining) {
+        if (this._creep.carry.energy < this._creep.carryCapacity && this.isMining) {
             let sources = this._creep.room.find(FIND_SOURCES);
             if (this._creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 this._creep.moveTo(sources[0]);
@@ -22,7 +22,7 @@ class UpgraderBehavior {
 
         // If we aren't at full carry capacity we aren't mining 
         // But if we aren't mining and we have no energy then we gonna mine dawg 
-        this.isMining = this._creep.carry.energy < 20 || (!this.isMining && this._creep.energy === 0);
+        this.isMining = (!this.isMining && this._creep.energy === 0) || this._creep.carry.energy < this._creep.carryCapacity;
 
         // we are DELIVERING 
         if (!this.isMining) {
